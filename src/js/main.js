be4c48c6e6e4e41de1404f7f0e25f0a9d5d8e09e
Lars Berglén <lars.berglen@ub.gu.se>
2018-03-14 19:11:45 +0100

    video n stuff

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,46 @@ var LUMBER = window.LUMBER || {};
 
 LUMBER.main = (function() {
   var init = function() {
-    console.log('new main!');
+    var vids = ['jim_punk_2', 'Reggae'];
+    var vid = vids[Math.floor(Math.random() * 2)];
+    var el = $('video');
+    var props = [{ 'suffix': '.mp4', 'type': 'mp4' },
+      { 'suffix': '.webm', 'type': 'webm' },
+      { 'suffix': '.ogv', 'type': 'ogg' }
+    ];
+
+    for (var i = 0; i < props.length; i++) {
+      var curr = props[i];
+      el.append($('<source src="videos/' + vid + curr.suffix + '" type="video/' + curr.type + '">'));
+
+    }
+
+
+    // timer
+    // Set the date we're counting down to
+    var countDownDate = new Date('Sep 1, 2018 10:00:00').getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now an the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      console.log(days + 'd ' + hours + 'h' +
+        minutes + 'm ' + seconds + 's ');
+      $('.timer').text(days + 'd ' + hours + 'h ' +
+        minutes + 'm ' + seconds + 's ');
+
+
+    }, 1000);
   };
 
   return {
